6725e05a61e36d2b0f548ce74e6252b83b27bc64
Himanshu Walia <himanshu.walia@gmail.com>
2018-01-11 07:22:47 +0100

    initial commits

---
+++ app/src/main/java/himanshuw/waddle/HomeActivity.java
@@ -0,0 +1,14 @@
+package himanshuw.waddle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import himanshuw.scriptorial_waddle.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
